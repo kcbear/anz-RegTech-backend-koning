5864014cbc02f399460a5418a5ee29175a7f07fb
kcbear <twinsen.sink@gmail.com>
2019-02-23 22:25:47 +1100

    commit repository classes and unit test

---
+++ src/main/java/anz/regtech/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package anz.regtech.repository;
+
+import anz.regtech.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    List<Account> findByNumber(String number);
+}
